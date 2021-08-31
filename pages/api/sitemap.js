b7957e89e24ee345cb90b29cfa58cebf3925e9c6
JohnnyBlomgren <johnny@gloot.com>
2021-08-31 12:32:16 +0200

    Updated sitemap

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -1,7 +1,7 @@
 export default function Sitemap(req, res) {
   const sitemap = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
   <url>
-    <loc>https://clipboard-parser.vercel.app</loc>
+    <loc>https://clipboard-parser.jblomgren.com</loc>
     <changefreq>daily</changefreq>
     <priority>1</priority>
   </url>
